8b4bbd97e176ca8fba04b381786623b09c95cbed
runawaygo <huangguan3112789@gmail.com>
2012-02-27 13:22:42 +0800

    compiled

---
+++ IG/compiled/asserts/walk.plist.soy.js
@@ -0,0 +1,11 @@
+// This file was automatically generated from monster.plist.soy.
+// Please don't edit this file by hand.
+
+goog.provide('lime.ASSETS.walk.plist');
+
+goog.require('soy');
+
+
+lime.ASSETS.walk.plist.data = function(opt_data) {
+  return '<?xml version="1.0" encoding="UTF-8"?> <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd"> <plist version="1.0"> <dict> <key>frames</key><dict><key>1.png</key> <dict> <key>x</key> <real>0</real> <key>y</key> <real>0</real> <key>width</key> <real>25</real> <key>height</key> <real>33</real> <key>offsetX</key> <real>0</real> <key>offsetY</key> <real>0</real> <key>originalWidth</key> <real>25</real> <key>originalHeight</key> <real>33</real> </dict><key>2.png</key> <dict> <key>x</key> <real>25</real> <key>y</key> <real>0</real> <key>width</key> <real>25</real> <key>height</key> <real>33</real> <key>offsetX</key> <real>0</real> <key>offsetY</key> <real>0</real> <key>originalWidth</key> <real>25</real> <key>originalHeight</key> <real>33</real> </dict><key>3.png</key> <dict> <key>x</key> <real>50</real> <key>y</key> <real>0</real> <key>width</key> <real>25</real> <key>height</key> <real>33</real> <key>offsetX</key> <real>0</real> <key>offsetY</key> <real>0</real> <key>originalWidth</key> <real>25</real> <key>originalHeight</key> <real>33</real> </dict> </dict> <key>texture</key> <dict> <key>width</key> <integer>25</integer> <key>height</key> <integer>33</integer> </dict> </dict> </plist>'
+};
